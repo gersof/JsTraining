5fc4eea81f2cbf537f6af798a19af337b7d41e0f
Juan E Angulo <jeangulo@workforcelogiq.com>
2020-03-10 11:44:16 -0500

    Se agrega pug y se crean unas cuantas vistas

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
+app.set('view engine', 'pug')
+// app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/view-with-pug', function (req, res) {
+    res.render('index', { title: 'Hola ', message: 'Hola Andres!' })
+  })
+
+  app.get('/products', function (req, res) {
+    res.render('products')
+  })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- nodejs/index.js
+++ nodejs/index.js
@@ -4,4 +4,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write('Hola Andres!'); //write a response to the client
   res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+}).listen(5589); //the server object listens on port 8080
