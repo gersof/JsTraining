294ee1594e626b0a6807de4d1eb6aad3ad57696e
Andres Gaviria <Andres.Gaviria@teaminternational.com>
2020-03-12 08:44:36 -0500

    cree los ejercicios falta enlazarla con html y css

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -1,13 +1,64 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+
+// // app.get('/', (req, res) =>{
+// //     res.send('hello andres');
+// });
 
 app.get('/sumar', (req, res) => {
-   let num1=parseInt( req.query.num1);  
-   let num2= parseInt( req.query.num2);  
-   let suma= num1 +num2;
-   res.json({Mensaje:'El resultado es '+suma});
-})
+   
+    let num1=parseInt( req.query.num1);  
+    let num2= parseInt( req.query.num2);
+     if (num1>5 && num2 >5) {
+         res.send('los numeros ingresados deben ser <= 5');
+     }else{let suma= num1 +num2;
+    res.json({Mensaje:'El resultado es '+suma});}
+    
+ });
+ 
+ app.get ('/restar', (req ,res)=>{
+ 
+    let num1 = parseInt(req.query.num1);
+    let num2 = parseInt(req.query.num2);
+    if (num1<num2){
+        res.send('el primer numero ingresado es menor que su substractor, ingresa uno mayor');
+    }else{
+            let resta = num1 - num2;
+    res.json(`el resultado de la resta es ${resta}`);
+    }
 
+ });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+ app.get('/multiplicar', (req, res)=>{
+     
+    let num1= parseInt(req.query.num1);
+    let num2 =parseInt(req.query.num2);
+    if (num1 === num2){
+        res.send('los numeros ingresados deben ser diferenes');
+    }else{
+        let multiplicar = num1 * num2;
+        res.json(`la multiplicacion de los numeros es ${multiplicar}`);
+    }
+ });
+
+ 
+
+ 
+ app.get('/dividir', (req, res)=>{
+     
+    let num1 = parseInt(req.query.num1);
+    let num2= parseInt(req.query.num2);
+    let dividir = num1 / num2;
+    if (dividir < 1){
+        res.send('el resultado es menor a 1, por favor ingresa otros valores ');
+    }else{
+        
+        res.json(`el resultado de la division es ${dividir}`);
+    }
+
+    app.use(express.static('presentacion'));
+ });
+
+app.listen(3000, ()=> {
+    console.log('server on port 3000');
+});
